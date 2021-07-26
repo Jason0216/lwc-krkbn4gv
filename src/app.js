2a8d5604137896fdf948993128f63a435ad3b006
Jason0216 <jason.e.mccarthy@gmail.com>
2021-07-26 16:30:00 +0000

    Updates

--- src/app.js
+++ src/app.js
@@ -1,27 +1,42 @@
-import { LightningElement } from "lwc";
+import { LightningElement,  wire, track, api } from 'lwc';
+import getCodes from '@salesforce/apex/DHA_Add_Codes_Class_Manager.getCodes';
 
-export default class App extends LightningElement {
-  title = "Welcome to Lightning Web Components!";
-
-  showFeatures = true;
-
-  /**
-   * Getter for the features property
-   */
-  get features() {
-    return [
-      {
-        label: "Learn in the browser.",
-        icon: "utility:edit",
-      },
-      {
-        label: "View changes to code instantly with Live Compilation.",
-        icon: "utility:refresh",
-      },
-      {
-        label: "Style your components with SLDS.",
-        icon: "utility:brush",
-      },
+const Columns = [
+    { label: 'Name', fieldName: 'Name', type: 'text' },
+    { label: 'Short Description', fieldName: 'Short_Description__c', type: 'textArea'},
+    { label: 'Body System', fieldName: 'Body_System__c', type: 'text'},
+    { label: 'Note', fieldName: 'User_Input_Text__c', type: 'textArea'}, 
     ];
-  }
+
+export default class SearchDataTable extends LightningElement {
+    @track searchedData = [];
+    @track codesAllData = [];
+    @track searchString;
+    codesColumns = Columns;
+    
+    connectedCallback() {
+    getCodes()
+        .then((result)=>{
+            this.codesAllData = result;
+        })
+        .catch((error)=>{
+            console.log('Error' + error);
+        })
+    }
+    searchDataTable(event){
+        var searchString =event.target.value.toUpperCase();
+        var allRecords =   this.codesAllData;
+        var searchResults =[];
+        var i;
+    
+        for(i=0; i<allRecords.length; i++){
+           if((allRecords[i].Name) && (allRecords[i].Name.toUpperCase().includes(searchString)) || 
+               (allRecords[i].Short_Description__c) && (allRecords[i].Short_Description__c.toUpperCase().includes(searchString)) ||
+               (allRecords[i].Body_System__c) && (allRecords[i].Body_System__c.toUpperCase().includes(searchString)) || 
+               (allRecords[i].User_Input_Text__c) && (allRecords[i].User_Input_Text__c.toUpperCase().includes(searchString)) ){
+                searchResults.push(allRecords[i]);
+            }
+        }
+        this.searchedData = searchResults;
+    }
 }
